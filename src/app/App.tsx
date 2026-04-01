f97caee26a7bedc74cd243a69f3237cf91fa080b
runohod <andrei.rumynthev1728@yandex.ru>
2026-04-01 22:18:33 +0300

    +

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
-import '@/app/styles';
-import './App.css';
+import '@/App/styles';
 
 function App() {
-return <h1>Hello World</h1>;
+  return (
+    <div className="app-container">
+      {/* Здесь будет Header с шагами 1-2-3 */}
+      {/* Здесь будет текущий шаг формы (тот, что на скрине) */}
+      {/* Здесь будут кнопки Back и Continue */}
+    </div>
+  );
 }
 
 export default App;
