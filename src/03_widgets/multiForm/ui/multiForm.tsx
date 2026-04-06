28a03b79053691add5b44464e858269c4bde4952
runohod <andrei.rumynthev1728@yandex.ru>
2026-04-06 21:06:51 +0300

    работа с основным файлом для страниц

--- src/03_widgets/multiForm/ui/multiForm.tsx
+++ src/03_widgets/multiForm/ui/multiForm.tsx
@@ -1,17 +1,32 @@
 import { LineRounded } from '@/06_shared/icons';
- 
-const MultiForm = () => {
-    return (
-        <div className="form">
-            <div className="breadCrumbs">
-                <svg>Personal Info</svg>
-                <LineRounded/>
-                <svg>Our services</svg>
-                <LineRounded/>
-                <svg>Payment</svg>
-            </div>
-        </div>
-    )
+import styles from './multiForm.module.scss';
+
+interface MultiFormProps { 
+    title: string;
+    subTitle: string;
+    children: React.ReactNode;
+    stepNumber: number
 }
 
+const MultiForm: React.FC<MultiFormProps> = ({ title, subTitle, children, stepNumber }) => {
+    return (
+        <div className={styles.formContainer}>
+            <div className={styles.breadCrumbs}>{stepNumber}</div>
+            <div className="formHeader">
+                <h1>{title}</h1>
+                <p>{subTitle}</p>
+            </div>
+            <main className="formContent">
+                {children}
+            </main>
+            <footer className={styles.formFooter}>
+                <button className={styles.backButton}>Back</button>
+                <button className={styles.continueButton}>Continue</button>
+            </footer>
+        </div>
+    );
+};
+
 export default MultiForm
+
+
