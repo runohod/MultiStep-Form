81d7a3006fa64646f5eed1ebd6b0103c86ef4437
runohod <andrei.rumynthev1728@yandex.ru>
2026-04-14 10:11:32 +0300

    +

--- src/03_widgets/multiForm/ui/multiFormStep-3.tsx
+++ src/03_widgets/multiForm/ui/multiFormStep-3.tsx
@@ -1,7 +1,22 @@
-// import styles from './multiFormStep-3.module.scss';
+import styles from './multiFormStep-3.module.scss';
 
 function MultiFormStep3() {
-    return (<div>1</div>)
+    return (
+    <div className={styles.formPay}>
+        <div className={styles.formCost}>
+            <div className={styles.result}>Итог к оплате:</div>
+            <div className={styles.money}>898₽</div>
+        </div>
+
+        <div className={styles.formContainer}>
+            <button className={styles.card}>Выбрать карту</button>
+        </div>
+
+        {/* <div className={styles.formContainer}>
+            <button className={styles.toPay}>Оплатить</button>
+        </div> */}
+    </div>
+    )
 };
 
 export {MultiFormStep3}
